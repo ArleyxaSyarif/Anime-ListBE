77953b88f2a4736b63fad0b56f8135ce98d11d70
Mohamad Arleyxa Syarif <kiritonkichan@gmail.com>
2026-03-01 04:51:46 +0700

    update tombol delete dan edit

--- app/Filament/Resources/Heroes/Tables/HeroesTable.php
+++ app/Filament/Resources/Heroes/Tables/HeroesTable.php
@@ -2,14 +2,13 @@
 
 namespace App\Filament\Resources\Heroes\Tables;
 
-use Filament\Actions\BulkActionGroup;
-use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
 use Filament\Tables\Table;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
-
 
 class HeroesTable
 {
@@ -17,9 +16,24 @@ class HeroesTable
     {
         return $table
             ->columns([
-                ImageColumn::make('image')->disk('public')->label('Hero Image'),
-                IconColumn::make('is_active')->boolean()->label('Aktif'),
-                TextColumn::make('created_at')->dateTime()->label('dibuat'),
+                ImageColumn::make('image')
+                    ->disk('public')
+                    ->label('Hero Image'),
+
+                IconColumn::make('is_active')
+                    ->boolean()
+                    ->label('Aktif'),
+
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->label('Dibuat'),
+            ])
+            ->actions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->bulkActions([
+                DeleteBulkAction::make(),
             ]);
     }
 }
